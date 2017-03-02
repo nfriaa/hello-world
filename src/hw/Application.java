755f5c4dbf5e6fa95124adecceea27cf7b36ae78
nafaa <nafaa@192.168.1.101>
2017-03-02 14:49:38 +0100

    initial commit

---
+++ src/hw/Application.java
@@ -0,0 +1,16 @@
+package hw;
+
+/**
+ *
+ * @author nafaa
+ */
+public class Application
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+}
